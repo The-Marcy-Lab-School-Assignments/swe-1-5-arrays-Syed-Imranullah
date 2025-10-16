90265fbb2b979d6a3af03602716fe10bfb804639
Syed <xayanmay@gmail.com>
2025-10-16 05:18:45 -0400

    finished 1- 8

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,26 +1,58 @@
-const addToFrontOrBack = () => {
+const addToFrontOrBack = (arr, value, isFront) => {
+  if (isFront) {
+    arr.unshift(value)
+  } else {
+    arr.push(value)
+  }
 };
 
-const reverseString = () => {
+const reverseString = (str) => {
+  const letters = str.split('')
+  const reversed = letters.reverse()
+  const joined = reversed.join('')
+  return joined
 };
 
-const newArrayFullOf = () => {
+const newArrayFullOf = (value, numOfValue) => {
+  const arr = []
+  for (let i = 0; i < numOfValue; i ++) {
+    arr.push(value)
+  } return arr
+
 };
 
-const insertIntoMiddle = () => {
+const insertIntoMiddle = (arr, value) => {
+  const middleIndex = Math.floor(arr.length / 2)
+  arr.splice(middleIndex, 0, value)
 };
 
-const deleteFromMiddle = () => {
+const deleteFromMiddle = (arr, value) => {
+  const middleIndex = Math.floor(arr.length / 2)
+  arr.splice(middleIndex, 1)
 };
 
-const isRightIndex = () => {
+
+const isRightIndex = (arr, value, index) => {
+  
+  if (arr[index] === value) {
+    return true
+  } else {
+    return false
+  }
 };
 
-const roundAllNumsDown = () => {
+const roundAllNumsDown = (arr) => {
+  return arr.map(num => Math.floor(num))
 };
 
-const getAllYCoordinates = () => {
-};
+const getAllYCoordinates = (arrOfCoords) => {
+  const result = []
+  for (let i = 0; i < arrOfCoords.length; i ++) {
+    const coordinate = arrOfCoords[i]
+    const y = coordinate[1]
+    result.push(y)
+  } return result 
+}; 
 
 module.exports = {
   addToFrontOrBack,
